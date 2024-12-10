1ffbea79e0c276641bb5031bef9ef9146e4cacd2
Nikolas-Svet <nikitaswetckin@mail.ru>
2024-12-10 16:40:13 +0700

    copy: clone project to_do_list_redux

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './assets/styles/styles.scss'
+import TodoList from './components/TodoList/TodoList'
+
+function App() {
+  return (
+    <div className="container__wrap">
+      <div className="container">
+        <TodoList />
+      </div>
+    </div>
+  )
+}
+
+export default App
